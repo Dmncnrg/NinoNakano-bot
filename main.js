5c7530e1533bcb695c2fd083f100ddc00ef7b8a5
Dominic Narag <60565226+Dmncnrg@users.noreply.github.com>
2022-07-06 20:11:08 +0800

    added events

--- main.js
+++ main.js
@@ -1,21 +1,52 @@
 const Discord = require('discord.js');
 
-require("dotenv").config()
+require("dotenv").config();
+
+const generateImage = require("./generateImage")
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 });
 
-client.on("ready", () =>{
-    console.log(`Logged in as ${client.user.tag}`);
-});
+let bot = {
+    client,
+    prefix: "n!",
+    owners: ["689641352195080208"]
+}
 
-client.on("messageCreate",(message) =>{
-    if(message.content == "hi"){
-        message.reply("Hello World!")
-    }
-})
+client.commands = new Discord.Collection()
+client.events = new Discord.Collection()
 
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
+client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
+
+client.loadEvents(bot, false)
+client.loadCommands(bot, false)
+
+module.exports = bot
+
+
+
+// client.on("ready", () =>{
+//     console.log(`Logged in as ${client.user.tag}`);
+// });
+
+// client.on("messageCreate",(message) =>{
+//     if(message.content == "hi"){
+//         message.reply("Hello World!")
+//     }
+// })
+
+// const welcomeChannelID = "957583483943923792";
+// client.on("guildMemberAdd", async (member)=>{
+//     const img = await generateImage(member);
+//     member.guild.channels.cache.get(welcomeChannelID).send({
+//         content: `<@${member.id}> Welcome to the server!`,
+//         files: [img]
+//     });
+//     // member.guild.channels.cache.get(welcomeChannelID).send(`<@${member.id}> Welcome to the server!`);
+// });
 client.login(process.env.TOKEN);
